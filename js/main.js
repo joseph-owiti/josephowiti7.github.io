186a3f01293e9fcde465aefdfb4d807510e3fb15
josephowiti7 <44106637+josephowiti7@users.noreply.github.com>
2021-01-07 16:41:41 -0500

    Nav Bar debug
    fixed projects.html nav bar issue

--- js/main.js
+++ js/main.js
@@ -125,29 +125,6 @@ $(document).ready(function(){
         scrollTop: $("project.html#f25").offset().top},'slow');
 }); 
 
-//$("#about").click(function() {
-//    $('html,body').animate({
-//        scrollTop: $("home.html#c").offset().top},'slow');
-//});   
-//$(".skills").click(function() {
-//    $('html,body').animate({
-//        scrollTop: $("home.html#d").offset().top},'slow');
-//}); 
-//$(".experience").click(function() {
-//    $('html,body').animate({
-//        scrollTop: $("home.html#g").offset().top},'slow');
-//}); 
-//$(".contact").click(function() {
-//    $('html,body').animate({
-//        scrollTop: $("home.html#h").offset().top},'slow');
-//}); 
-//$(".home").click(function() {
-//    $('html,body').animate({
-//        scrollTop: $("home.html").offset().top},'slow');
-//});
-    
-
-
 
                 
 
---
+++ js/projects.js
@@ -0,0 +1,21 @@
+$("#about").click(function() {
+    $('html,body').animate({
+        scrollTop: $("home.html#c").offset().top},'slow');
+});   
+$(".skills").click(function() {
+    $('html,body').animate({
+        scrollTop: $("home.html#d").offset().top},'slow');
+}); 
+$(".experience").click(function() {
+    $('html,body').animate({
+        scrollTop: $("home.html#g").offset().top},'slow');
+}); 
+$(".contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $("home.html#h").offset().top},'slow');
+}); 
+$(".home").click(function() {
+    $('html,body').animate({
+        scrollTop: $("home.html").offset().top},'slow');
+});
+    
